55788278be29d5ec4f02c54f33f4aa0f24d4f925
OkuSan - Paratpanu Pechsaman <paratpanu18@gmail.com>
2026-02-12 15:59:26 +0700

    add cell03

---
+++ cell03/ex00/upto25.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+num = int(input("Enter a number less than 25\n"))
+
+if num > 25:
+    print("Error")
+    exit()
+
+while num <= 25:
+    print(f'Inside the loop, my variable is {num}')
+    num += 1
